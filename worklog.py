efde2d0b5fa947ef42f3c910661aca8cebcd86f5
ha1t <halt.feits@gmail.com>
2013-12-20 13:52:12 +0900

    日付をまたいで動かしてもうまいこと動くように

--- worklog.py
+++ worklog.py
@@ -2,7 +2,7 @@ from time import sleep
 import datetime
 import subprocess
 import os
-outdir = 'screencapture'
+outdir = '../../screencapture'
 
 now = datetime.datetime.today()
 counter = 1
@@ -10,7 +10,12 @@ counter = 1
 def capture():
     global counter
     global now
-    filename = '%s%02d%02d_%08d.png' % (now.year, now.month, now.day, counter)
+
+    if now.day != datetime.datetime.today().day:
+        now = datetime.datetime.today()
+        counter = 1
+
+    filename = '%s%02d%02d_%05d.png' % (now.year, now.month, now.day, counter)
     outpath = os.path.join(outdir, filename)
     subprocess.call(['screencapture', '-x', '-C', outpath])
     counter += 1
