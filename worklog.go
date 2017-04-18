733544310624f1d467e3ef6dde740b3904a7bc57
ha1t <halt.feits@gmail.com>
2017-04-18 12:37:05 +0900

    fix variable names

--- worklog.go
+++ worklog.go
@@ -35,19 +35,19 @@ func capture() {
 	filename := fmt.Sprintf("%d%02d%02d_%05d.png", now.Year(), now.Month(), now.Day(), counter)
 
 	cmd := "/usr/sbin/screencapture"
-	cmd_option := "/Users/tsuruoka-n/screencapture/" + filename
+	cmdOption := "/Users/tsuruoka-n/screencapture/" + filename
 	// fmt.Println(cmd + cmd_option) // 表示
 
-	_, err := exec.Command(cmd, "-x", "-C", cmd_option).Output()
+	_, err := exec.Command(cmd, "-x", "-C", cmdOption).Output()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 }
 
-func getCounter(r_counter int) int {
+func getCounter(counterNumberer int) int {
 
-	if r_counter == 1 {
+	if counterNumberer == 1 {
 		// すでにファイルがあるか調べてあるなら一番大きい値を使う
 		filename := fmt.Sprintf("%d%02d%02d_*", now.Year(), now.Month(), now.Day())
 		items, err := filepath.Glob("/Users/tsuruoka-n/screencapture/" + filename)
@@ -56,16 +56,16 @@ func getCounter(r_counter int) int {
 		}
 		// アイテムないなら最初から
 		if len(items) == 0 {
-			return r_counter
+			return counterNumberer
 		}
 		sort.Strings(items)
-		string_suffix := strings.Split(items[len(items)-1], "_")[1]
-		latest_number, _ := strconv.Atoi(strings.Split(string_suffix, ".")[0])
+		stringSuffix := strings.Split(items[len(items)-1], "_")[1]
+		latestNumber, _ := strconv.Atoi(strings.Split(stringSuffix, ".")[0])
 
-		r_counter = latest_number + 1
+		counterNumberer = latestNumber + 1
 	} else {
-		r_counter++
+		counterNumberer++
 	}
 
-	return r_counter
+	return counterNumberer
 }
